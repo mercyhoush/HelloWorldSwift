9532299aa07a5a1de25f39b60f86e86a74e89b90
Mercy Housh <mercyhoush@Mercys-MacBook-Pro.local>
2019-06-07 18:16:34 -0500

    First commit

---
+++ SwiftHelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftHelloWorld
+//
+//  Created by Mercy Housh on 6/7/19.
+//  Copyright © 2019 Mercy Housh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
